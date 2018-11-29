d2cae1b9ce5956926512c4bfe1f4e36e0c7810a6
jsfish <hello_jinyu@163.com>
2018-11-29 15:41:25 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {Provider} from "react-redux";
-import {createStore} from "redux";
-import reducers from "./models/reducers";
-import Routes from "./router";
-import css from "./index.css";
-import "whatwg-fetch";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './models/reducers';
+import Routes from './router';
+import css from './index.css';
+import 'whatwg-fetch';
+
 const store = createStore(reducers);
 ReactDOM.render(
-    <Provider store={store}>
-         <Routes />
-     </Provider> ,
-    document.getElementById("app")
-)
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById('app'),
+);
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,18 +1,87 @@
-import React, { Component } from 'react';
-import Component1 from '../components/Component1';
+import React, { useState, useEffect } from 'react';
+import sqlFormatter from 'sql-formatter';
+import { Button, Row, Col } from 'antd';
+import brace from 'brace';
+import 'brace/theme/sqlserver';
+import 'brace/mode/mysql';
 
-class IndexPage extends Component {
-  constructor(props) {
-    super();
-  }
+import AceEditor from 'react-ace';
 
-  render() {
-    return (
-      <div>
-IndexPage
-        <div><Component1 /></div>
-      </div>
-    );
-  }
-}
+
+const handleOnload = (e) => {
+  // console.log('load', e);
+};
+const IndexPage = () => {
+  const [formatSql, setFormatSql] = useState('');
+  const [beforeSql, setBeforeSql] = useState('');
+  const handleFormatSql = () => {
+    const newFormatSql = sqlFormatter.format(beforeSql);
+    setFormatSql(newFormatSql);
+  };
+  const handleEditorChange = (value) => {
+    setBeforeSql(value);
+  };
+  return (
+    <div className="sql-formatter">
+      <Button type="primary" style={{ textAlign: 'center', margin: '0 auto 20px', display: 'block' }} onClick={handleFormatSql}>格式化SQL</Button>
+      <Row type="flex" justify="space-around">
+        <Col className="editorContainer" span={11}>
+          <AceEditor
+            mode="mysql"
+            theme="sqlserver"
+            name="blah2"
+            onLoad={handleOnload}
+            onChange={handleEditorChange}
+            fontSize={14}
+            height="1000px"
+            showPrintMargin
+            showGutter
+            highlightActiveLine
+            value={beforeSql}
+            setOptions={{
+              enableBasicAutocompletion: false,
+              enableLiveAutocompletion: false,
+              enableSnippets: false,
+              showLineNumbers: true,
+              tabSize: 2,
+            }}
+          />
+
+        </Col>
+        <Col className="editorContainer" span={11}>
+          <AceEditor
+            mode="mysql"
+            theme="sqlserver"
+            name="blah3"
+            height="1000px"
+            onLoad={handleOnload}
+            onChange={handleEditorChange}
+            fontSize={14}
+            showPrintMargin
+            showGutter
+            highlightActiveLine
+            value={formatSql}
+            setOptions={{
+              enableBasicAutocompletion: false,
+              enableLiveAutocompletion: false,
+              enableSnippets: false,
+              showLineNumbers: true,
+              tabSize: 2,
+            }}
+          />
+        </Col>
+      </Row>
+      <style jsx global>{`
+        .sql-formatter {
+          width:100%;
+          padding:10px 20px;
+        } 
+        .editorContainer {
+          border: 1px dashed grey;
+        }
+      `}
+      </style>
+    </div>
+  );
+};
 export default IndexPage;
